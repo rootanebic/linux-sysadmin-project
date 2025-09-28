a4697808ef6a7c50b4664844255a5f7c4a0a63d7
Amel <nebic_amel_it@hotmail.com>
2025-09-28 21:27:09 +0200

    Added bash script for system monitoring health-check.sh

---
+++ configs/health-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# health-check.sh
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+MEM=$(free -m | awk 'NR==2{printf "%s/%sMB", $3,$2 }')
+DISK=$(df -h / | awk 'NR==2{print $3"/"$2}')
+USERS=$(who | wc -l)
+FAILED_LOGINS=$(grep 'Failed password' /var/log/auth.log | wc -l)
+
+
+echo "$DATE CPU: $CPU% MEM: $MEM DISK: $DISK USERS: $USERS FAILED_LOGINS: $FAILED_LOGINS" >> /var/log/health-check.log
